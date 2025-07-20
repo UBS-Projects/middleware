e2283d8342f2258e33dbbdcb0ccaa7285b93481c
Your Name <you@example.com>
2025-07-20 15:55:48 +0300

    Updated route editing to use Kaoto and integrated with new backend API

---
+++ src/main/java/com/middleware/backend/kaotocamel/config/CamelRestConfig.java
@@ -0,0 +1,15 @@
+package com.middleware.backend.kaotocamel.config;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CamelRestConfig extends RouteBuilder {
+    @Override
+    public void configure() {
+        restConfiguration()
+                .component("servlet")
+                .contextPath("/")
+                .port(8081);  // تأكد نفس المنفذ
+    }
+}
