77b303755321bb8870bea0e93a3656f87b87d801
Your Name <you@example.com>
2025-08-28 16:28:58 +0300

    cleaned code

--- src/main/java/com/middleware/backend/workflow/processors/StepProcessor.java
+++
@@ -1,8 +0,0 @@
-package com.middleware.backend.workflow.processors;
-
-import org.apache.camel.Exchange;
-import com.middleware.backend.model.WorkflowStep;
-
-public interface StepProcessor {
-    void process(Exchange exchange, WorkflowStep step) throws Exception;
-}
