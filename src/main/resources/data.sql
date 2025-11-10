7b10ff5039d8af533e617b77c33f42edd449a637
Mohammad Obeid <mohammadkadoumi77@yahoo.com>
2025-11-10 09:48:26 +0200

    Update authentication method to application; clear user password on Keycloak update; modify user insert statement in data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -130,10 +130,10 @@ INSERT INTO public.permissions (id, name) VALUES
 
 
 INSERT INTO public.users
-(created_by, created_at, updated_by, updated_at, email, status, user_name)
+(created_by, created_at, updated_by, updated_at, email, password, status, user_name)
 VALUES
     ('SYSTEM',NOW(),'SYSTEM',NOW(), 'admin@mail.com',
-
+     '$2a$10$xBgaTDblE6qOZAAuJsMVWeWfuqifp8TyNGmxKeKp6JB3zDxh6gdIe',
      'ACTIVE',
      'admin')
     ON CONFLICT (email) DO NOTHING;
